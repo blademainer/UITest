39f69b8bc1fc64e88ea2a1ee67bffe42926070c7
blademainer <blademainer@gmail.com>
2014-04-13 22:42:40 +0800

    新增.gitignore

--- src/main/java/com/xiongyingqi/MainActivity.java
+++ src/main/java/com/xiongyingqi/MainActivity.java
@@ -1,14 +1,14 @@
 package com.xiongyingqi;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends Activity
-{
-    /** Called when the activity is first created. */
+public class MainActivity extends FragmentActivity {
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
